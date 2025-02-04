367c485c1a9ad94301ca03b24e49019d62584893
arjun-s <arjun-s@arjun-ss-MacBook-Pro.local>
2025-02-04 17:53:58 +0530

    Added a file for constants and added the language selector dropdown

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,7 +2,9 @@
 import Link from "next/link";
 import { TbHexagonLetterAFilled } from "react-icons/tb";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue, } from "@/components/ui/select"; 
+import { Language_Versions } from "@/app/Constants";
 
+const languages = Object.entries(Language_Versions);
 const Navbar = () => {
   return (
     <nav className="bg-[#1B1C1D] text-white p-2">
@@ -18,7 +20,7 @@ const Navbar = () => {
         </Link>
 
          {/* File menu */}
-         <div className="flex justify-between items-center w-full">
+         <div className="flex justify-between items-center ml-2 mr-6">
             <Select>
                <SelectTrigger className="w-[70px] bg-transparent border-none text-white hover:bg-[#2D2E2F]">
                   <SelectValue placeholder="File" className="font-normal text-[16px]" />
@@ -30,6 +32,27 @@ const Navbar = () => {
                   <SelectItem value="save" className="flex justify-between">
                      <span>Save</span> 
                   </SelectItem>
+               </SelectContent>
+            </Select>
+         </div>
+
+         {/* Language Dropdown */}
+         <div className="flex justify-between items-center w-full ml-2 mr-6">
+            <Select>
+               <SelectTrigger className=" capitalize w-[150px] bg-transparent border-none text-white hover:bg-[#2d2e2f]">
+                  <SelectValue placeholder="Languages" className=" font-normal text-[16px]" />
+               </SelectTrigger>
+               <SelectContent className="bg-white text-black">
+                  {
+                     languages.map(([language, version]) => (
+                        <SelectItem 
+                           key={language} 
+                           value={language}
+                           className="capitalize flex justify-between items-center"
+                           >{language}{`   ${version}`}
+                        </SelectItem>
+                     ))
+                  }
                </SelectContent>
             </Select>
          </div>
